980f318eddbadafed270f3e30e78493759962ef5
Leo Giertz <leogiertz@slapintosh.sthlm.doberman.se>
2010-01-30 18:19:08 +0100

    Fixed so thata table view is present in the tab bar controller.

---
+++ Classes/TracksListTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  TracksListTableViewController.h
+//  BuddyJ
+//
+//  Created by Leo Giertz on 100130.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface TracksListTableViewController : UITableViewController {
+    NSArray* tracks;
+}
+
+@end
